0804a0590ee87b88b6a43d57fd4a4b628ef8527a
Nikita <nikitaprotska@gmail.com>
2023-10-11 12:09:46 +0300

    Программа написана, работает коректно

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] usserArray = new string[5] { "Hello", "2", "world", ":-)", "123" };
+string[] printArray = new string[usserArray.Length];
+void SecondArray(string[] usserArray, string[] printArray)
+{
+    int count = 0;
+    for (int i = 0; i < usserArray.Length; i++)
+    {
+        if (usserArray[i].Length <= 3)
+        {
+            printArray[count] = usserArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(usserArray, printArray);
+PrintArray(printArray);
